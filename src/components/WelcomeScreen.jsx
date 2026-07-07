6ff739b08617b9e878cb968b027751752711b84b
findmussa <findmussa@gmail.com>
2026-07-07 14:53:22 +0100

    fix: update version badge to v1.0.0 in WelcomeScreen

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -672,7 +672,7 @@ export default function WelcomeScreen({
 
           <div className={s.nameRow}>
             <h1 className={s.appName}>FlowUrja Studio</h1>
-            <span className={s.version}>v0.1.0</span>
+            <span className={s.version}>v1.0.0</span>
           </div>
 
           <p className={s.tagline}>
